3a6ec9a471f859cd10f0c1ab1ef6e604e6b6f4cb
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-09-29 21:50:19 +0100

    fix: add TR to routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "pl", "it"],
+  locales: ["en", "pl", "it", "tr"],
   defaultLocale: "pl",
   localePrefix: "as-needed"
 })
